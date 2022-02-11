31cb5566dad794f4cc5900cfc84ca5bec25bb797
lilei <1339098678@qq.com>
2022-02-11 20:49:00 +0800

    edit list cmd description

--- cmd/list.go
+++ cmd/list.go
@@ -18,14 +18,9 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: listRun,
+	Short: "List the todos",
+	Long:  `Listing the todos`,
+	Run:   listRun,
 }
 
 func init() {
